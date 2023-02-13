fb512331d0e5227007f74e69f5a4d40dca4c195f
xionghao <18829037917@163.com>
2023-02-13 11:27:19 +0800

    Update 去掉无用import

--- eureka_learn/gm-order/src/main/java/com/gua/gm/controller/OrderController.java
+++ eureka_learn/gm-order/src/main/java/com/gua/gm/controller/OrderController.java
@@ -10,7 +10,6 @@ import com.gua.gm.pojo.qo.OrderQO;
 import com.gua.gm.service.IOrderService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
