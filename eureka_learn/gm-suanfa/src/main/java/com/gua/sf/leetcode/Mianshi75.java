8c71339c75149e80a883cad02a0fc9abb7f2b713
xionghao <18829037917@163.com>
2023-04-29 22:38:58 +0800

    Update Mianshi75.java
    拥有最多糖果的孩子

--- eureka_learn/gm-suanfa/src/main/java/com/gua/sf/leetcode/Mianshi75.java
+++ eureka_learn/gm-suanfa/src/main/java/com/gua/sf/leetcode/Mianshi75.java
@@ -1,5 +1,7 @@
 package com.gua.sf.leetcode;
 
+import java.util.List;
+
 public class Mianshi75 {
 
     /**
@@ -61,6 +63,36 @@ public class Mianshi75 {
             remainder = a % b;
         }
         return b;
+    }
+
+    /**
+     *
+     1431. 拥有最多糖果的孩子
+     给你一个数组 candies 和一个整数 extraCandies ，其中 candies[i] 代表第 i 个孩子拥有的糖果数目。
+
+     对每一个孩子，检查是否存在一种方案，将额外的 extraCandies 个糖果分配给孩子们之后，此孩子有 最多 的糖果。注意，允许有多个孩子同时拥有 最多 的糖果数目。
+
+
+     示例1
+
+     输入：candies = [2,3,5,1,3], extraCandies = 3
+     输出：[true,true,true,false,true]
+     解释：
+     孩子 1 有 2 个糖果，如果他得到所有额外的糖果（3个），那么他总共有 5 个糖果，他将成为拥有最多糖果的孩子。
+     孩子 2 有 3 个糖果，如果他得到至少 2 个额外糖果，那么他将成为拥有最多糖果的孩子。
+     孩子 3 有 5 个糖果，他已经是拥有最多糖果的孩子。
+     孩子 4 有 1 个糖果，即使他得到所有额外的糖果，他也只有 4 个糖果，无法成为拥有糖果最多的孩子。
+     孩子 5 有 3 个糖果，如果他得到至少 2 个额外糖果，那么他将成为拥有最多糖果的孩子。
+
+     来源：力扣（LeetCode）
+     链接：https://leetcode.cn/problems/kids-with-the-greatest-number-of-candies
+     著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        // 1.求出数组最大值
+        // 2.遍历数组,判断每个值+额外糖果数>=数组最大值
+
+        return null;
     }
 
 
