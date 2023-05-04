22e04a0ef090afd89a4de9c7c40243ee80a1f00b
xionghao <18829037917@163.com>
2023-05-04 10:02:19 +0800

    Update Mianshi75.java

--- eureka_learn/gm-suanfa/src/main/java/com/gua/sf/leetcode/Mianshi75.java
+++ eureka_learn/gm-suanfa/src/main/java/com/gua/sf/leetcode/Mianshi75.java
@@ -1,5 +1,6 @@
 package com.gua.sf.leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Mianshi75 {
@@ -66,33 +67,92 @@ public class Mianshi75 {
     }
 
     /**
-     *
-     1431. 拥有最多糖果的孩子
-     给你一个数组 candies 和一个整数 extraCandies ，其中 candies[i] 代表第 i 个孩子拥有的糖果数目。
-
-     对每一个孩子，检查是否存在一种方案，将额外的 extraCandies 个糖果分配给孩子们之后，此孩子有 最多 的糖果。注意，允许有多个孩子同时拥有 最多 的糖果数目。
-
-
-     示例1
-
-     输入：candies = [2,3,5,1,3], extraCandies = 3
-     输出：[true,true,true,false,true]
-     解释：
-     孩子 1 有 2 个糖果，如果他得到所有额外的糖果（3个），那么他总共有 5 个糖果，他将成为拥有最多糖果的孩子。
-     孩子 2 有 3 个糖果，如果他得到至少 2 个额外糖果，那么他将成为拥有最多糖果的孩子。
-     孩子 3 有 5 个糖果，他已经是拥有最多糖果的孩子。
-     孩子 4 有 1 个糖果，即使他得到所有额外的糖果，他也只有 4 个糖果，无法成为拥有糖果最多的孩子。
-     孩子 5 有 3 个糖果，如果他得到至少 2 个额外糖果，那么他将成为拥有最多糖果的孩子。
-
-     来源：力扣（LeetCode）
-     链接：https://leetcode.cn/problems/kids-with-the-greatest-number-of-candies
-     著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * 1431. 拥有最多糖果的孩子
+     * 给你一个数组 candies 和一个整数 extraCandies ，其中 candies[i] 代表第 i 个孩子拥有的糖果数目。
+     * <p>
+     * 对每一个孩子，检查是否存在一种方案，将额外的 extraCandies 个糖果分配给孩子们之后，此孩子有 最多 的糖果。注意，允许有多个孩子同时拥有 最多 的糖果数目。
+     * <p>
+     * <p>
+     * 示例1
+     * <p>
+     * 输入：candies = [2,3,5,1,3], extraCandies = 3
+     * 输出：[true,true,true,false,true]
+     * 解释：
+     * 孩子 1 有 2 个糖果，如果他得到所有额外的糖果（3个），那么他总共有 5 个糖果，他将成为拥有最多糖果的孩子。
+     * 孩子 2 有 3 个糖果，如果他得到至少 2 个额外糖果，那么他将成为拥有最多糖果的孩子。
+     * 孩子 3 有 5 个糖果，他已经是拥有最多糖果的孩子。
+     * 孩子 4 有 1 个糖果，即使他得到所有额外的糖果，他也只有 4 个糖果，无法成为拥有糖果最多的孩子。
+     * 孩子 5 有 3 个糖果，如果他得到至少 2 个额外糖果，那么他将成为拥有最多糖果的孩子。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode.cn/problems/kids-with-the-greatest-number-of-candies
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      */
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         // 1.求出数组最大值
         // 2.遍历数组,判断每个值+额外糖果数>=数组最大值
+        int max = 0;
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] > max) {
+                max = candies[i];
+            }
+        }
+        List<Boolean> kidsWithCandies = new ArrayList<>();
+        for (int i = 0; i < candies.length; i++) {
+            kidsWithCandies.add(candies[i] + extraCandies >= max);
+        }
+        return kidsWithCandies;
+    }
 
-        return null;
+    /**
+     * 605. 种花问题
+     * 假设有一个很长的花坛，一部分地块种植了花，另一部分却没有。可是，花不能种植在相邻的地块上，它们会争夺水源，两者都会死去。
+     * <p>
+     * 给你一个整数数组 flowerbed 表示花坛，由若干 0 和 1 组成，其中 0 表示没种植花，1 表示种植了花。另有一个数 n ，能否在不打破种植规则的情况下种入 n 朵花？能则返回 true ，不能则返回 false 。
+     * <p>
+     * <p>
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：flowerbed = [1,0,0,0,1], n = 1
+     * 输出：true
+     * 示例 2：
+     * <p>
+     * 输入：flowerbed = [1,0,0,0,1], n = 2
+     * 输出：false
+     */
+
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+
+        /**
+         * 遍历数组，
+         * 仅a(i-1)=0,a(i)=0,a(i+1)=0时，可种花数量+1
+         * 然后把a(i)=1，继续遍历
+         *
+         * [0,0,0,0,0]
+         *
+         * [1,0,0,0,1]
+         *
+         * [1,0,0,1,0,0,1]
+         *
+         * [1,0,1,0,1,0,1]
+         */
+
+        int number = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            int locate = flowerbed[i];
+            if (locate == 1) {
+                continue;
+            }
+            int before = i == 0 ? 0 : flowerbed[i - 1];
+            int after = i == flowerbed.length - 1 ? 0 : flowerbed[i + 1];
+
+            if (before == 0 && after == 0 && locate == 0) {
+                flowerbed[i] = 1;
+                number++;
+            }
+        }
+        return number >= n;
     }
 
 
@@ -107,5 +167,10 @@ public class Mianshi75 {
 
         String gcdOfStrings = m.gcdOfStrings("abc", "abcabc");
         System.out.println(gcdOfStrings);
+
+
+        int[] intArray = {1,0,0};
+        boolean canPlaceFlowers = m.canPlaceFlowers(intArray, 1);
+        System.out.println(canPlaceFlowers);
     }
 }
