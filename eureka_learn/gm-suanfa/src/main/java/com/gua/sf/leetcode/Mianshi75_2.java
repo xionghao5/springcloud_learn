a1521ec236ed8f4627ae62074d009edcc4cc799a
xionghao <18829037917@163.com>
2023-06-04 12:07:59 +0800

    Update Mianshi75_2.java
    队列，把不满足条件的出队列，这样可以得到满足条件的元素的个数

--- eureka_learn/gm-suanfa/src/main/java/com/gua/sf/leetcode/Mianshi75_2.java
+++ eureka_learn/gm-suanfa/src/main/java/com/gua/sf/leetcode/Mianshi75_2.java
@@ -518,6 +518,60 @@ public class Mianshi75_2 {
 
     }
 
+    /**
+     933. 最近的请求次数
+     写一个 RecentCounter 类来计算特定时间范围内最近的请求。
+
+     请你实现 RecentCounter 类：
+
+     RecentCounter() 初始化计数器，请求数为 0 。
+     int ping(int t) 在时间 t 添加一个新请求，其中 t 表示以毫秒为单位的某个时间，并返回过去 3000 毫秒内发生的所有请求数（包括新请求）。确切地说，返回在 [t-3000, t] 内发生的请求数。
+     保证 每次对 ping 的调用都使用比之前更大的 t 值。
+
+
+
+     示例 1：
+
+     输入：
+     ["RecentCounter", "ping", "ping", "ping", "ping"]
+     [[], [1], [100], [3001], [3002]]
+     输出：
+     [null, 1, 2, 3, 3]
+
+     解释：
+     RecentCounter recentCounter = new RecentCounter();
+     recentCounter.ping(1);     // requests = [1]，范围是 [-2999,1]，返回 1
+     recentCounter.ping(100);   // requests = [1, 100]，范围是 [-2900,100]，返回 2
+     recentCounter.ping(3001);  // requests = [1, 100, 3001]，范围是 [1,3001]，返回 3
+     recentCounter.ping(3002);  // requests = [1, 100, 3001, 3002]，范围是 [2,3002]，返回 3
+     */
+
+
+    /**
+     * 分析
+     * 1.用队列存储请求时间
+     * 2.调用时，把时间放入队列
+     * 3.然后计算开发时间，t-3000
+     * 4.把队列中时间<t-3000的=请求时间出队列，返回队列的长度
+     */
+    private final Queue<Integer> pingTimeQueue = new LinkedList<>();
+
+    public int ping(int t) {
+        pingTimeQueue.offer(t);
+
+        int begin = t - 3000;
+
+        while (true) {
+            Integer time = pingTimeQueue.peek();
+            if (time < begin) {
+                pingTimeQueue.poll();
+            }else {
+                break;
+            }
+        }
+        return pingTimeQueue.size();
+    }
+
     public static void main(String[] args) {
         Mianshi75_2 m = new Mianshi75_2();
         boolean b = m.uniqueOccurrences(new int[]{1, 2, 2, 1, 1, 3});
@@ -542,5 +596,13 @@ public class Mianshi75_2 {
         Assert.isTrue(Arrays.equals(new int[]{5, 10}, asteroidCollision), "算法错误");
 
         Assert.isTrue("accaccacc".equals(m.decodeString("3[a2[c]]")), "算法错误");
+
+        int[] pingArray = new int[]{1, 100, 3001, 3002};
+        int[] pingResultArray = new int[pingArray.length];
+        for (int j = 0; j < pingArray.length; j++) {
+            pingResultArray[j] = m.ping(pingArray[j]);
+        }
+        Assert.isTrue(Arrays.equals(new int[]{1, 2, 3, 3}, pingResultArray), "算法错误");
+
     }
 }
