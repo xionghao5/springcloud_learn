8b0433d15581ab3132cd46aeca7a78fcf4383629
xionghao <18829037917@163.com>
2023-06-16 16:02:55 +0800

    Update Miasnshi75_3.java
    递归，计算当前节点，返回左右节点

--- eureka_learn/gm-suanfa/src/main/java/com/gua/sf/leetcode/Miasnshi75_3.java
+++ eureka_learn/gm-suanfa/src/main/java/com/gua/sf/leetcode/Miasnshi75_3.java
@@ -182,6 +182,67 @@ public class Miasnshi75_3 {
         getLeafListFromTree(list, treeNode.right);
     }
 
+
+    /**
+     * 1448. 统计二叉树中好节点的数目
+     * 给你一棵根为 root 的二叉树，请你返回二叉树中好节点的数目。
+     * <p>
+     * 「好节点」X 定义为：从根到该节点 X 所经过的节点中，没有任何节点的值大于 X 的值。
+     * <p>
+     * <p>
+     * <p>
+     * 示例 1：
+     * <p>
+     * <p>
+     * <p>
+     * 输入：root = [3,1,4,3,null,1,5]
+     * 输出：4
+     * 解释：图中蓝色节点为好节点。
+     * 根节点 (3) 永远是个好节点。
+     * 节点 4 -> (3,4) 是路径中的最大值。
+     * 节点 5 -> (3,4,5) 是路径中的最大值。
+     * 节点 3 -> (3,1,3) 是路径中的最大值。
+     * 示例 2：
+     * <p>
+     * <p>
+     * <p>
+     * 输入：root = [3,3,null,4,2]
+     * 输出：3
+     * 解释：节点 2 -> (3, 3, 2) 不是好节点，因为 "3" 比它大。
+     * 示例 3：
+     * <p>
+     * 输入：root = [1]
+     * 输出：1
+     * 解释：根节点是好节点。
+     */
+
+    public int goodNodes(TreeNode root) {
+        /**
+         * 分析：采用递归算法
+         * 1.递归方法
+         * 2.用max记录最大值
+         * 3.用sum记录好节点数量
+         * 4.当前节点的好节点数量=当前节点是否是好节点+左子节点的好节点数量+右子节点的好节点数量
+         */
+        return getNodes(root, root.val);
+
+    }
+
+    public int getNodes(TreeNode treeNode, int max) {
+        if (treeNode == null) {
+            return 0;
+        }
+        int sum = 0;
+        int val = treeNode.val;
+        if (val >= max) {
+            sum++;
+            max = val;
+        }
+        sum = sum + getNodes(treeNode.left, max);
+        sum = sum + getNodes(treeNode.right, max);
+        return sum;
+    }
+
     public static void main(String[] args) {
 
         Integer[] array = {3, 9, 20, null, null, 15, 7};
@@ -200,5 +261,11 @@ public class Miasnshi75_3 {
         TreeNode leafSimilarTreeNode1 = m.arrayToTreeNode(leafSimilarArray1);
         TreeNode leafSimilarTreeNode2 = m.arrayToTreeNode(leafSimilarArray2);
         Assert.isTrue(m.leafSimilar(leafSimilarTreeNode1, leafSimilarTreeNode2), "算法错误");
+
+
+        Integer[] goodNodesArray = {3, 1, 4, 3, null, 1, 5};
+        TreeNode goodNodesTreeNode = m.arrayToTreeNode(goodNodesArray);
+        int goodNodesResult = m.goodNodes(goodNodesTreeNode);
+        Assert.isTrue(4 == goodNodesResult, "算法错误");
     }
 }
