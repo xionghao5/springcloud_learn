9f98e90e49e55fa08bd4c2b303f26b0eb58c5eeb
xionghao <18829037917@163.com>
2023-06-22 21:55:40 +0800

    Update Miasnshi75_3.java
    二叉树的最近公共祖先

--- eureka_learn/gm-suanfa/src/main/java/com/gua/sf/leetcode/Miasnshi75_3.java
+++ eureka_learn/gm-suanfa/src/main/java/com/gua/sf/leetcode/Miasnshi75_3.java
@@ -2,10 +2,7 @@ package com.gua.sf.leetcode;
 
 import org.springframework.util.Assert;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * 面试必考精华版，75 题覆盖全量考点
@@ -35,6 +32,9 @@ public class Miasnshi75_3 {
         }
     }
 
+    public TreeNode constractTreeNode(int val) {
+        return new TreeNode(val);
+    }
 
     public TreeNode arrayToBinaryTree(Integer[] array) {
         if (array == null || array.length == 0) {
@@ -431,6 +431,71 @@ public class Miasnshi75_3 {
         }
     }
 
+    /**
+     * 236. 二叉树的最近公共祖先
+     * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+     * <p>
+     * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个节点 p、q，最近公共祖先表示为一个节点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+     * <p>
+     * <p>
+     * <p>
+     * 示例 1：
+     * <p>
+     * <p>
+     * 输入：root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+     * 输出：3
+     * 解释：节点 5 和节点 1 的最近公共祖先是节点 3 。
+     * 示例 2：
+     * <p>
+     * <p>
+     * 输入：root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+     * 输出：5
+     * 解释：节点 5 和节点 4 的最近公共祖先是节点 5 。因为根据定义最近公共祖先节点可以为节点本身。
+     * 示例 3：
+     * <p>
+     * 输入：root = [1,2], p = 1, q = 2
+     * 输出：1
+     * <p>
+     * <p>
+     * 提示：
+     * <p>
+     * 树中节点数目在范围 [2, 105] 内。
+     * -109 <= Node.val <= 109
+     * 所有 Node.val 互不相同 。
+     * p != q
+     * p 和 q 均存在于给定的二叉树中。
+     */
+
+    Map<Integer, TreeNode> parentMap = new HashMap<>();
+    Set<Integer> visitedSet = new HashSet<>();
+
+    public void dfsFunction(TreeNode treeNode) {
+        TreeNode left = treeNode.left;
+        if (left != null) {
+            parentMap.put(left.val, treeNode);
+            dfsFunction(left);
+        }
+        TreeNode right = treeNode.right;
+        if (right != null) {
+            parentMap.put(right.val, treeNode);
+            dfsFunction(right);
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        dfsFunction(root);
+        while (p != null) {
+            visitedSet.add(p.val);
+            p = parentMap.get(p.val);
+        }
+        while (q != null) {
+            if (visitedSet.contains(q.val)) {
+                return q;
+            }
+            q = parentMap.get(q.val);
+        }
+        return null;
+    }
 
     public static void main(String[] args) {
 
@@ -470,5 +535,13 @@ public class Miasnshi75_3 {
 
 
         Assert.isTrue(4 == m.longestZigZag2(m.arrayToBinaryTree(longestZigZagArray)), "算法错误");
+
+
+        Integer[] lowestCommonAncestorArray = {3, 5, 1, 6, 2, 0, 8, null, null, 7, 4};
+        TreeNode lowestCommonAncestorTreeNode = m.arrayToTreeNode(lowestCommonAncestorArray);
+        TreeNode p = m.constractTreeNode(5);
+        TreeNode q = m.constractTreeNode(4);
+        TreeNode lowestCommonAncestorResult = m.lowestCommonAncestor(lowestCommonAncestorTreeNode, p, q);
+        Assert.isTrue(p.val == lowestCommonAncestorResult.val, "算法错误");
     }
 }
